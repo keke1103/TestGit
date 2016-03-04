6e7703b94badafe6b0e42e33b15c630515725a07
keke1103 <240430306@qq.com>
2016-03-04 22:58:47 +0800

    第二次开发

--- src/com/keke/testgit/MainActivity.java
+++ src/com/keke/testgit/MainActivity.java
@@ -1,34 +1,41 @@
 package com.keke.testgit;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends FragmentActivity {
+
+	View b1;
+	View b2;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		b1 = findViewById(R.id.but1);
+		b2 = findViewById(R.id.but2);
+		b1.setOnClickListener(l);
+		b2.setOnClickListener(l);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+	private OnClickListener l = new OnClickListener() {
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		@Override
+		public void onClick(View v) {
+			Intent intent;
+			switch (v.getId()) {
+			case R.id.but2:
+				intent = new Intent(getApplicationContext(), KekeActivity.class);
+				startActivity(intent);
+				break;
+
+			default:
+				break;
+			}
+
 		}
-		return super.onOptionsItemSelected(item);
-	}
+	};
 }
