b4f1bcca045b037085e2f0f92358e5719415d1bd
keke1103 <240430306@qq.com>
2016-03-05 00:17:44 +0800

    最终

--- src/com/keke/testgit/KekeActivity.java
+++ src/com/keke/testgit/KekeActivity.java
@@ -1,7 +1,11 @@
 package com.keke.testgit;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AnalogClock;
 import android.widget.Chronometer;
 
@@ -10,6 +14,7 @@ public class KekeActivity extends Activity {
 	Chronometer mTime;
 	AnalogClock mClock;
 
+	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -18,5 +23,18 @@ public class KekeActivity extends Activity {
 		mTime = (Chronometer) findViewById(R.id.time);
 		mClock = (AnalogClock) findViewById(R.id.analogClock1);
 		mClock.animate().start();
+
+		mClock.setOnClickListener(l);
 	}
+
+	OnClickListener l = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			Intent intent = getIntent();
+			intent.setClass(KekeActivity.this, SetActivity.class);
+			startActivity(intent);
+
+		}
+	};
 }
--- src/com/keke/testgit/MainActivity.java
+++ src/com/keke/testgit/MainActivity.java
@@ -33,6 +33,8 @@ public class MainActivity extends FragmentActivity {
 				break;
 
 			default:
+				intent = new Intent(getApplicationContext(), SetActivity.class);
+				startActivity(intent);
 				break;
 			}
 
